80d5d943722b9e361b61a9f47bd3725d3391c7ec
Barış Sokat <baris.sokat@innoviayazilim.com>
2024-04-21 15:40:11 +0300

    Add apis to fetch data from db and streaming

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,25 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+export default async function CardWrapper(props: any) {
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Collected"
+        value={props.totalPaidInvoices}
+        type="collected"
+      />
+      <Card title="Pending" value={props.totalPendingInvoices} type="pending" />
+      <Card
+        title="Total Invoices"
+        value={props.numberOfInvoices}
+        type="invoices"
+      />
       <Card
         title="Total Customers"
-        value={numberOfCustomers}
+        value={props.numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
